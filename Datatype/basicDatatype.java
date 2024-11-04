510867b4b12d7cdae479d574ea52e4980cd44a39
Piyush <piyushmsdhoni07@gmail.com>
2024-11-04 12:02:27 +0530

    Basics about the data types in java

---
+++ Datatype/basicDatatype.java
@@ -0,0 +1,55 @@
+public class basicDatatype{
+    public static void main(String[] args){
+
+        /*
+        * There are two type of data types in java programming language
+        * 1. Primitive Datatype
+        * 2. Non-Primitive Datatype
+        * 
+        * 1. Primitive Datatype:-
+        *          a. Boolean : Stores true or false values (8 bits)
+        *          b. Char : Stores characters  (16 bits)
+        *          c. Byte : stores numbers -128 to 127 (8 bits)
+        *          d. Short : Stores numbers -32768 to 32767 (16 bits)
+        *          e. Int  : Stores numbers (32 bits)
+        *          f. long : Stores numbers (64 bits)
+        *          g. Float : Stores decimal numbers upto 7 decimal digits(32 bits)
+        *          h. Double : Stores decimal numbers upto 16 decimal digits (64 bits)
+        */
+
+        // Syntax of boolean 
+            boolean flag = true;
+        
+        // Syntax of char
+            char input = 'a';
+
+        // Syntax of byte
+            byte numByte = 1;
+
+        // Syntax of short
+            short numShort = 2;
+
+        // Syntax of int 
+            int numInt = 3;
+
+        // Syntax of long
+            long numLong = 4;
+
+        // Syntax of float
+            float numFloat = 5.6f;
+
+        //Syntax of double 
+            double numDouble = 7.8;
+
+
+        System.out.println(flag);
+        System.out.println(input);
+        System.out.println(numByte);
+        System.out.println(numShort);
+        System.out.println(numInt);
+        System.out.println(numLong);
+        System.out.println(numFloat);
+        System.out.println(numDouble);
+
+    }
+}
