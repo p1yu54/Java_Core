502f20480c4af40c4b314eb84fffdac5c17e0785
Piyush <piyushmsdhoni07@gmail.com>
2024-11-12 23:28:40 +0530

    Starting basics of loops in Java

---
+++ loops/basicOfLoops.java
@@ -0,0 +1,43 @@
+public class basicOfLoops {
+    public static void main(String[] args) {
+        /*
+         *  
+         *   There are two types of loops in java
+         *          a. Entry controlled loops
+         *          b. Exit controlled loops 
+         *          
+         * 
+         *      1. Entry controlled loops:-
+         *                     In these loops condition given to execute the loop is checked before the loop is executed
+         *                     if the condition is true then the loop is executed otherwise the loop is not executed
+         * 
+         *                      a. For loop
+         *                      b. While Loop
+         *          
+         *          a.For loop:
+         *                    Syntax : for(initialization(Starting of loop) ; condition(End point of the loop) ; unary operator ){
+         *                                  //statement;
+         *                             }
+         * 
+         *          b.While Loop:
+         *                     Syntax : while(condition){
+         *                                  //statement;
+         *                                  unary operator(as per the need of the program)
+         *                              }
+         *      
+         *      2. Exit Controlled loops:-
+         *                         In those the loop is executed once before the condition given is checked and if after the condtion
+         *                         is true the loop will execute till the condtion is achieved of if is false the loop will no longer
+         *                         run.
+         * 
+         *          a. Do-While Loop:   
+         *                Syntax : do{
+         *                              //statement
+         *                              unary operator(as per the need of the program)            
+         *                         }                                                 
+         *                         while(condition);                                   
+         * 
+         * 
+         */
+    }
+}
