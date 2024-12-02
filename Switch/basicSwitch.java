078f44de16c89258063fe042557deb514869a9f3
Piyush <piyushmsdhoni07@gmail.com>
2024-12-02 20:29:48 +0530

    Basics of Switch Statement

---
+++ Switch/basicSwitch.java
@@ -0,0 +1,24 @@
+public class basicSwitch{
+    public static void main(String[] args) {
+        /*
+         * 
+         * Switch case is used to check the expression given to it and accordingly check the conditions given to it
+         * 
+         * : Syntax
+         *      switch(expression){
+         *          case 1 : 
+         *                  //body
+         *                  break; // it is used to break the case so it will not run futher in the program
+         * 
+         *          case 2 :
+         *                  //body
+         *                  break;
+         * 
+         *          default :
+         *                  //body 
+         *                  break; // if the soultion of the expression does not match any of the cases given to the switch body
+         *                              then the block of default will automatically be executed 
+         *      }
+         */
+    }
+}
