f6c4f951d15a230adcebd8e22356f60ccbacfd02
Piyush <piyushmsdhoni07@gmail.com>
2024-12-06 23:44:31 +0530

    Making and calling the constructor of two different classes

---
+++ OOps/Encapsulation/Encapsulation2/Encapsulation2.java
@@ -0,0 +1,16 @@
+class A{
+    A(){
+        System.out.println("Welcome to India");
+    }
+}
+class B{
+    B(){
+        System.out.println("How are you");
+    }
+}
+public class Encapsulation2 {
+    public static void main(String[] args) {
+        new A();
+        new B();
+    }
+}
