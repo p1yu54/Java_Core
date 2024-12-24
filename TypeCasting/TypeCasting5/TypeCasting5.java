188a9f8899bf0b515c1e8fde9b18a52158f28c72
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 00:36:30 +0530

    TypeCasting float to double

---
+++ TypeCasting/TypeCasting5/TypeCasting5.java
@@ -0,0 +1,13 @@
+// TypeCasting float to double
+
+public class TypeCasting5 {
+    public static void main(String[] args) {
+        float a = 69f;
+
+        // converting float to double 
+        double b = a;
+
+        System.out.println("This is float = " + a);
+        System.out.println("This is double = " + b);
+    }
+}
