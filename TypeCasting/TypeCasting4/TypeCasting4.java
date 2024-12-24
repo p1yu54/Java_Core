3e42f491c2517888a90874c07a4ed4b65e668910
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 00:33:47 +0530

    TypeCasting long to float and double

---
+++ TypeCasting/TypeCasting4/TypeCasting4.java
@@ -0,0 +1,17 @@
+// TypeCasting long to float and double
+
+public class TypeCasting4 {
+    public static void main(String[] args) {
+        long a = 69;
+        
+        // converting long to float 
+        float b = a;
+
+        // converting long to double 
+        double c = a;
+
+        System.out.println("This is long = " + a);
+        System.out.println("This is float = " + b);
+        System.out.println("This is double = " + c);
+    }
+}
