460fb37b87ae8b83d6f519884ea3b4575fca38c3
Piyush <piyushmsdhoni07@gmail.com>
2024-12-24 13:17:46 +0530

    Making a first file and accessing its class and  methods into the main method

---
+++ OOps/Aggregation/Aggregation1/A.java
@@ -0,0 +1,7 @@
+// This is first file whose class and methods will be acccessed into the main method
+
+public class A{
+    void show(){
+        System.out.println("Hello World!!");
+    }
+}
---
+++ OOps/Aggregation/Aggregation1/Aggregation1.java
@@ -0,0 +1,10 @@
+// Making a first file and accessing its class and  methods into the main method
+
+
+public class Aggregation1{
+    public static void main(String[] args) {
+        // accessing the class and methods of the first file
+        A a = new A();
+        a.show();
+    }
+}
