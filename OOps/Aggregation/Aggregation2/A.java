6034e773a8cc73034d13aa4f312bf6b218ed8f5d
Piyush <piyushmsdhoni07@gmail.com>
2024-12-24 13:25:26 +0530

    Accessing the default as well as parameterised constructor made in the first file in the main method

---
+++ OOps/Aggregation/Aggregation2/A.java
@@ -0,0 +1,10 @@
+// Making default as well as parameterised constructor in this file that would later be called in the main file
+
+public class A {
+    A(){
+        System.out.println("This is A class constructor");
+    }
+    A(int a){
+        System.out.println("The value of a = " + a);
+    }
+}
---
+++ OOps/Aggregation/Aggregation2/Aggregation2.java
@@ -0,0 +1,8 @@
+// Accessing the default as well as parameterised constructor made in the first file in the main method
+
+public class Aggregation2 {
+    public static void main(String[] args) {
+        new A();
+        new A(43);
+    }
+}
