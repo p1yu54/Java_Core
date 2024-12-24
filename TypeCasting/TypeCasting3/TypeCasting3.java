f283d8c846b82170cb871aa816ea97ba63858e71
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 00:30:54 +0530

    TypeCasting integer from long to double

---
+++ TypeCasting/TypeCasting3/TypeCasting3.java
@@ -0,0 +1,21 @@
+// TypeCasting integer from long to double
+
+public class TypeCasting3 {
+    public static void main(String[] args) {
+        int a = 69;
+
+        // converting int to long
+        long b = a;
+
+        // coverting int  to float 
+        float c = a;
+
+        // converting int to double 
+        double d = a;
+
+        System.out.println("This is integer = " + a);
+        System.out.println("This is long = " + b);
+        System.out.println("This is float = " + c);
+        System.out.println("This is double = " + d);
+    }    
+}
