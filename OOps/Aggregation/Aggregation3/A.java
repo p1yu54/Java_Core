5c6711aa097957a8e781e23283cca91a417a2a83
Piyush <piyushmsdhoni07@gmail.com>
2024-12-24 13:31:20 +0530

    Accessing the getter setter methods made in the first file in the main method

---
+++ OOps/Aggregation/Aggregation3/A.java
@@ -0,0 +1,13 @@
+// Making getter setter method in this file for the instance variable 
+
+public class A {
+    int a ;
+    
+    void setA(int a){
+        this.a = a;
+    }
+
+    int getA(){
+        return a;
+    }
+}
---
+++ OOps/Aggregation/Aggregation3/Aggregation3.java
@@ -0,0 +1,9 @@
+// Accessing the getter setter methods made in the first file in the main method
+
+public class Aggregation3 {
+    public static void main(String[] args) {
+        A a = new A();
+        a.setA(98);
+        System.out.println(a.getA());
+    }    
+}
