f76cd113a58c0aebb7157cfbc1e6999e6e7f6cd8
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 00:12:45 +0530

    Accessing the static class method and variable from the first file 'A' in main method

---
+++ OOps/Aggregation/Aggregation-10/A.java
@@ -0,0 +1,10 @@
+// Making Static class method and variable 
+
+public class A {
+    static class B{
+        static int a = 90;
+        static void show(){
+            System.out.println("The value of static variable = " + a);
+        }
+    }
+}
---
+++ OOps/Aggregation/Aggregation-10/Aggregation10.java
@@ -0,0 +1,7 @@
+// Accessing the static class method and variable from the first file 'A' in main method
+
+public class Aggregation10 {
+    public static void main(String[] args) {
+        A.B.show();
+    }
+}
