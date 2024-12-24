6a82dd76a864cf8a27bf2c543653984f7cd3110d
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 00:21:49 +0530

    TypeCasting byte from short to double

---
+++ TypeCasting/TypeCasting1/TypeCasting1.java
@@ -0,0 +1,28 @@
+// TypeCasting byte from short to double
+public class TypeCasting1{
+    public static void main(String[] args) {
+        byte a = 98;
+
+        // converting byte into short
+        short b = a;
+
+        // converting byte into integer
+        int c = a;
+
+        // converting byte into long
+        long d = a;
+
+        // convertiing byte into float
+        float e = a;
+
+        // convertinh byte into double
+        double f = e;
+
+        System.out.println("This is byte = " + a);
+        System.out.println("This is short = " + b);
+        System.out.println("This is int = " + c);
+        System.out.println("This is long = " + d);
+        System.out.println("This is float = " + e);
+        System.out.println("This is double = " + f);
+    }
+}
