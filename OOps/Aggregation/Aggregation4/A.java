82b207ec0c8da0cada5a2390b39f888f45b2c403
Piyush <piyushmsdhoni07@gmail.com>
2024-12-24 15:08:27 +0530

    Accessing method of class child class as well as parent class through child class's object in main method

---
+++ OOps/Aggregation/Aggregation4/A.java
@@ -0,0 +1,12 @@
+// Achieveing Single Level Inheritance 
+
+class B{
+    void show(){
+        System.out.println("This is method of B class which is parent class");
+    }
+}
+public class A extends B {
+    void show1(){
+        System.out.println("This is method of A class which is child class");
+    }
+}
---
+++ OOps/Aggregation/Aggregation4/Aggregation4.java
@@ -0,0 +1,9 @@
+// Accessing method of class child class as well as parent class through child class's object in main method
+
+public class Aggregation4 {
+    public static void main(String[] args) {
+        A a = new A();
+        a.show();
+        a.show1();
+    }
+}
