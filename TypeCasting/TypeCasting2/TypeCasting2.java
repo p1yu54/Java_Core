87da02ff2fd4130d15ced00f4b7993dc2948b7bb
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 00:26:19 +0530

    TypeCasting short fron integer to double

---
+++ TypeCasting/TypeCasting2/TypeCasting2.java
@@ -0,0 +1,25 @@
+// TypeCasting short fron integer to double
+
+public class TypeCasting2 {
+    public static void main(String[] args) {
+        short a = 87;
+
+        // converting short into int
+        int b = a;
+
+        // converting short into long
+        long c = a;
+
+        // converting short into float 
+        float d = a;
+
+        // converting short into double
+        double e = a;
+
+        System.out.println("This is short = " + a);
+        System.out.println("This is integer = " + b);
+        System.out.println("This is long = " + c);
+        System.out.println("This is float = " + d);
+        System.out.println("This is double = " + e);
+    }
+}
