82ef1a1c6d4513a730ce1e2a1908042f09c8aee8
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 00:03:54 +0530

    Accesing the final class , method and variable in the main method through aggregation

---
+++ OOps/Aggregation/Aggregation9/A.java
@@ -0,0 +1,8 @@
+// Making final class method and variable 
+
+public final class A {
+    final void show(){
+        final int a = 34;
+        System.out.println("The value of final variable a = " + a);
+    }
+}
---
+++ OOps/Aggregation/Aggregation9/Aggregation9.java
@@ -0,0 +1,8 @@
+// Accesing the final class , method and variable in the main method through aggregation
+
+public class Aggregation9 {
+    public static void main(String[] args) {
+        A a = new A();
+        a.show();
+    }
+}
