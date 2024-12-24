1e99a4eb46af4a78c7e5a6eec0b24af62bf86ed6
Piyush <piyushmsdhoni07@gmail.com>
2024-12-24 23:51:39 +0530

    Accessing (Overload of) methods from another class in the first file 'A'

---
+++ OOps/Aggregation/Aggregation7/A.java
@@ -0,0 +1,10 @@
+// Overload methods which will be accessed in the main file
+
+public class A {
+    void show(){
+        System.out.println("This is show method of class A");
+    }    
+    void show(int a){
+        System.out.println("The value of a = " + a);
+    }
+}
---
+++ OOps/Aggregation/Aggregation7/Aggregation7.java
@@ -0,0 +1,9 @@
+// Accessing (Overload of) methods from another class in the first file 'A'
+
+public class Aggregation7{
+    public static void main(String[] args) {
+        A a = new A();
+        a.show();
+        a.show(87);
+    }
+}
