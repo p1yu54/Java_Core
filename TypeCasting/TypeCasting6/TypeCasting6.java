364348d313880553e2978cd93d370952a2019dbb
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 12:39:12 +0530

    TypeCasting double

---
+++ TypeCasting/TypeCasting6/TypeCasting6.java
@@ -0,0 +1,34 @@
+// TypeCasting double
+
+public class TypeCasting6 {
+    public static void main(String[] args) {
+        double a = 69;
+
+
+        // converting double to float 
+        float b = (float) a;
+
+        // coverting double to long
+        long c = (long) a;
+
+        // coverting double to int
+        int d = (int) a;
+        
+        // converting double to char
+        char e = (char) a;
+
+        // converting double to short
+        short f = (short) a;
+
+        // converting double to byte
+        byte g = (byte) a;
+
+        System.out.println("This is double = " + a);
+        System.out.println("This is float = " + b);
+        System.out.println("This is long = " + c);
+        System.out.println("This is int = " + d);
+        System.out.println("This is char = " + e);
+        System.out.println("This is short = " + f);
+        System.out.println("This is byte = " + g);
+    }
+}
