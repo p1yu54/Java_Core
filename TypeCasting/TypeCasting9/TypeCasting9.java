bb1d63e7e13837a310cbd331f83100367395c0c3
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 12:55:16 +0530

    TypeCasting int

---
+++ TypeCasting/TypeCasting9/TypeCasting9.java
@@ -0,0 +1,21 @@
+// TypeCasting int
+
+public class TypeCasting9 {
+    public static void main(String[] args) {
+         int a = 67;
+
+         // converting int to char 
+         char b = (char)a;
+ 
+         // converting int to short
+         short c = (short) a;
+ 
+         // converting int to byte
+         byte d = (byte) a;
+ 
+         System.out.println("This is int = " + a);
+         System.out.println("This is char = " + b);
+         System.out.println("This is short = " + c);
+         System.out.println("This is byte = " + d);
+    }
+}
