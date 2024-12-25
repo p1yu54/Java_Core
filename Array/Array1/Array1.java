b1e9d906d1313cf6b73c1ca8c2fedd01a3e2f246
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 22:20:19 +0530

    Printing an array using a for loop

---
+++ Array/Array1/Array1.java
@@ -0,0 +1,11 @@
+// Printing an array using a for loop
+
+public class Array1{
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4,5,6,7,8,9,10};
+
+        for(int i = 0 ; i < a.length ; i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+}
