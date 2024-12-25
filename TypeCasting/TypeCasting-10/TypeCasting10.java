b019583f135d19df3dd0e3697bc8d6c687112947
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 12:58:44 +0530

    TypeCasting char

---
+++ TypeCasting/TypeCasting-10/TypeCasting10.java
@@ -0,0 +1,17 @@
+// TypeCasting char
+
+public class TypeCasting10 {
+    public static void main(String[] args) {
+         char a = 'c';
+ 
+         // converting char to short
+         short b = (short) a;
+ 
+         // converting char to byte
+         byte c = (byte) a;
+ 
+         System.out.println("This is char = " + a);
+         System.out.println("This is short = " + b);
+         System.out.println("This is byte = " + c);
+    }
+}
