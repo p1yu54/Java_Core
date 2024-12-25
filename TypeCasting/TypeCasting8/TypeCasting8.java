491fb432a7063eb270b1fbc3f4c617383458a774
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 12:50:14 +0530

    TypeCasting long

---
+++ TypeCasting/TypeCasting8/TypeCasting8.java
@@ -0,0 +1,25 @@
+// TypeCasting long
+
+public class TypeCasting8 {
+    public static void main(String[] args) {
+        long a = 78;
+
+        // converting long to int 
+        int b = (int)a;
+
+        // converting long to char 
+        char c = (char)a;
+
+        // converting long to short
+        short d = (short) a;
+
+        // converting long to byte
+        byte e = (byte) a;
+
+        System.out.println("This is long = " + a);
+        System.out.println("This is int = " + b);
+        System.out.println("This is char = " + c);
+        System.out.println("This is short = " + d);
+        System.out.println("This is byte = " + e);
+    }
+}
