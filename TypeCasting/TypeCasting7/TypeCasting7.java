3388e2989fa614579a48185611de80ce4d5ce340
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 12:45:06 +0530

    TypeCasting Float

---
+++ TypeCasting/TypeCasting7/TypeCasting7.java
@@ -0,0 +1,29 @@
+// TypeCasting Float
+
+public class TypeCasting7 {
+    public static void main(String[] args) {
+        float a = 69f;
+
+        // converting float to long
+        long b = (long)a;
+
+        // converting float to int 
+        int c = (int) a;
+
+        // converting float to char 
+        char d = (char) a;
+
+        // converting float to short 
+        short e = (short)a;
+
+        // convertinh float to byte
+        byte f = (byte)a;
+
+        System.out.println("This is float = " + a);
+        System.out.println("This is long = " + b);
+        System.out.println("This is int = " + c);
+        System.out.println("This is char = " + d);
+        System.out.println("This is short = " + e);
+        System.out.println("This is byte = " + f);
+    }    
+}
