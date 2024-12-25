012c936051a7b1976e14e5d44b6d3c78bc9d4a1e
Piyush <piyushmsdhoni07@gmail.com>
2024-12-25 13:02:14 +0530

    TypeCasting short

---
+++ TypeCasting/TypeCasting-11/TypeCasting11.java
@@ -0,0 +1,13 @@
+// TypeCasting short
+
+public class TypeCasting11 {
+    public static void main(String[] args) {
+        short a = 69 ;
+
+        // converting short to byte
+        byte b = (byte) a;
+
+        System.out.println("This is short = " + a);
+        System.out.println("This is byte = " + b);
+    }
+}
