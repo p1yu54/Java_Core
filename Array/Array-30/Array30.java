59ee23d862bed088b15cf1258a4098f8884ae450
Piyush <piyushmsdhoni07@gmail.com>
2025-01-10 15:18:41 +0530

    Finding the maximum element in an array

---
+++ Array/Array-30/Array30.java
@@ -0,0 +1,35 @@
+// Finding the maximum element in an array 
+
+import java.util.Scanner;
+
+public class Array30 {
+    public static int[] inputArray(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of your array :- ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        for(int i = 0 ; i < size ; i++){
+            System.out.print("Enter the element at the index " + (i+1) + " of your array :- ");
+            arr[i] = sc.nextInt();
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int a[] = inputArray();
+        
+        int max = a[0];
+
+        for(int i = 0 ; i < a.length ; i++){
+            if(a[i] > max){
+                max = a[i];
+            }
+        }
+
+        System.out.println("The maximum number of your array is " + max);
+    }
+}
